705c5d619f5b99ad1b952d111aa65519e779552b
dev-harsh1998 <harshitjain6751@gmail.com>
2018-09-03 01:14:34 +0530

    RMX1801: new derpy way to bring back gcc
    * Since ANDROID_BUILD_TOP is dead and BUILD_TOP isn't working as expected
    
    * Refrence https://i.kym-cdn.com/photos/images/newsfeed/000/096/044/trollface.jpg?1296494117
    
    Signed-off-by: dev-harsh1998 <harshitjain6751@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,6 +54,8 @@ BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_PAGE_SIZE)
 TARGET_KERNEL_ARCH := arm64
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 # TARGET_KERNEL_CLANG_COMPILE := true
+KERNEL_TOOLCHAIN := $(OUT_DIR)/../prebuilts/gcc/$(HOST_OS)-x86/aarch64/linaro-6.5.0/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 TARGET_KERNEL_SOURCE := kernel/realme/sdm660
 TARGET_KERNEL_CONFIG := sdm660-perf_defconfig
 
